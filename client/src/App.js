2c1d3ce250a361922306f6bcd675586bca37e02e
Madhu Ravuri <madhurrr@Madhus-MacBook-Pro.local>
2020-09-21 21:49:06 -0700

    Mock up of Main.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Main from "./views/Main";
-import Palette from "./views/Palette";
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <Palette />
     </div>
   );
 }
