e63a85fae7e3402687d06731fba608ea7d8efccb
Madhu Ravuri <madhurrr@Madhus-MacBook-Pro.local>
2020-09-21 17:38:22 -0700

    Basic framework started

---
+++ client/src/views/Palette.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { navigate } from "@reach/router";
+import axios from "axios";
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "http://www.colourlovers.com/api/palettes/random";
+export default () => {
+  /*let [colorOne, setColorOne] = useState({});
+  let [colorTwo, setColorTwo] = useState({});
+  let [colorThree, setColorThree] = useState({});
+  let [colorFour, setColorFour] = useState({});
+  let [colorFive, setColorFive] = useState({});*/
+
+  useEffect(() => {
+    axios.get(proxyurl + url).then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
+  /*const setInitialColors = () => {
+    setColorOne(getRandomColor());
+    setColorTwo(getRandomColor());
+    setColorThree(getRandomColor());
+    setColorFour(getRandomColor());
+    setColorFive(getRandomColor());
+  };*/
+  return <div></div>;
+};
