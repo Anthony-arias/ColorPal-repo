ad2fa831d1dd21ff9648cc2b745601f9a2018b1f
Madhu Ravuri <madhurrr@Madhus-MacBook-Pro.local>
2020-09-24 00:33:16 -0700

    Styled reg/login, added button to main

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
 import Main from "./views/Main";
-import Register from "./components/UserForm"
+import Register from "./components/UserForm";
 import { Router } from "@reach/router";
 
 function App() {
   return (
-      <div className="App">
-          <Router>
-              <Main path="/" />
-              <Register path="/register" />
-          </Router>
+    <div className="App">
+      <Router>
+        <Main path="/" />
+        <Register path="/register" />
+      </Router>
     </div>
   );
 }
